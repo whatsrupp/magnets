e9a05bd3f5745477e3ca57e983e5924a15f88447
whatsrupp <nickrupp95@gmail.com>
2018-04-06 11:05:08 +0100

    Add provisional sidebar styling

--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import logo from './magnet.svg'
+import './Sidebar.css';
 
 class Sidebar extends Component {
 
   render(){
     return(
       <div className="Sidebar" >
-      <img src={logo} className="Sidebar-logo" alt="logo" />
+      <img src={logo} className="Sidebar__Logo" alt="logo" />
+          <img src={logo} className="Sidebar__Logo" alt="logo" />
       </div>
     )
   }
