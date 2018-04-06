6294c180ce16f035fd399c3e1d765f7ec770b83d
whatsrupp <nickrupp95@gmail.com>
2018-04-06 11:04:38 +0100

    Customise App Component

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Sidebar from 'Sidebar/Sidebar';
 
@@ -7,14 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <Sidebar />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Sidebar />
       </div>
     );
   }
